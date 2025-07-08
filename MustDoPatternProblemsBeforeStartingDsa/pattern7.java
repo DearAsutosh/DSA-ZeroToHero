d6e230363e5b1ef4fe0587982c2926a27300956e
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-07-08 18:48:56 +0530

    add pattern7

---
+++ MustDoPatternProblemsBeforeStartingDsa/pattern7.java
@@ -0,0 +1,30 @@
+package MustDoPatternProblemsBeforeStartingDsa;
+
+public class pattern7 {
+    public static void pattern(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern7.pattern(5);
+    }
+}
+
+// OUTPUT
+// ==========
+// 1
+// 12
+// 123
+// 1234
+// 12345

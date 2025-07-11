0b3a2afa5416ba0edf911072db6c854c06ec7ed5
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-07-11 20:35:44 +0530

    add pattern-17

--- MustDoPatternProblemsBeforeStartingDsa/pattern17.java
+++ MustDoPatternProblemsBeforeStartingDsa/pattern17.java
@@ -1,27 +1,38 @@
-package MustDoPatternProblemsBeforeStartingDsa;
+ package MustDoPatternProblemsBeforeStartingDsa;
 
 public class pattern17 {
-    static void pattern(int n) {
-        char ch='A';
+    static void pattern(int n){
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print(ch);
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
             }
-            ch++;
+            char ch='A';
+            int breakpoint=(2*i+1)/2;
+            for (int j = 0; j < 2*i +1; j++) {
+                System.out.print(ch);
+                if (j<breakpoint) {
+                    ch++;
+                }else{
+                    ch--;
+                }
+            }
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+
+            }
             System.out.println();
         }
-    }
 
+    }
     public static void main(String[] args) {
         pattern17.pattern(5);
-
     }
 }
 
-// OUTPUT
-// =======
-// A
-// BB
-// CCC
-// DDDD
-// EEEEE
+//output
+// =========
+//     A    
+//    ABA
+//   ABCBA
+//  ABCDCBA
+// ABCDEDCBA

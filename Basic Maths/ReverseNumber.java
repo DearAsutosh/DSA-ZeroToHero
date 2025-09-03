26d1c1e59300014fb34e493a78c411d46c6e3f09
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-09-03 21:17:09 +0530

    add reverse a number

--- Basic Maths/ReverseNumber.java
+++ Basic Maths/ReverseNumber.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ReverseNumber {
+    static int Solution(int a) {
+        int res = 0;
+        while (a != 0) {
+           int ld=a%10;
+            res =(res*10)+ld;
+            a/=10;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        
+        int num;
+        System.out.println("Enter a number :");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        System.out.println(Solution(num));
+        sc.close();
     }
 }

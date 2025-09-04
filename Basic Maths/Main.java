8a969d7225e7349790ecf1f7fe85d3cfcc44d9ea
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-09-04 23:55:23 +0530

    add palindrome number

---
+++ Basic Maths/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean palindrome(int n) {
+        int revNum = 0;
+        int dup = n;
+        while (n > 0) {
+            int ld = n % 10;
+            revNum = (revNum * 10) + ld;
+            n = n / 10;
+        }
+        if (dup == revNum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter a number :");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        if (palindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+    }
+}

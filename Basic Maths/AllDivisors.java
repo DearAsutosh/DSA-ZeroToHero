70a5ad191eae4df376c4f8ea27f588b953651ae3
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-09-24 19:08:14 +0530

    add MOST OPTIMIZED APPROACH to print allDivisors

--- Basic Maths/AllDivisors.java
+++ Basic Maths/AllDivisors.java
@@ -4,21 +4,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AllDivisors {
-    public static List<Integer> Solution(int n) {
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                nums.add(i);
-                int pair = n / i;
-                if (pair != i) {
-                    nums.add(pair);
+    //===========BRUTE-FORCE APPROACH==================
+    // public static List<Integer> Solution(int n) {
+    //     ArrayList<Integer> nums = new ArrayList<Integer>();
+    //     for (int i = 1; i <= Math.sqrt(n); i++) {
+    //         if (n % i == 0) {
+    //             nums.add(i);
+    //             int pair = n / i;
+    //             if (pair != i) {
+    //                 nums.add(pair);
+    //             }
+    //         }
+    //     }
+    //     Collections.sort(nums);
+    //     return nums;
+    // }
+
+    //=========OPTIMIZED APPROACH==============
+    public static List<Integer> Solution(int n){
+        List<Integer> small=new ArrayList<>();
+        List<Integer> large=new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n) ; i++) {
+            if(n%i==0){
+                small.add(i);
+                if (i!=n/i) {
+                    large.add(0,n/i);
                 }
             }
         }
-        Collections.sort(nums);
-        return nums;
+        small.addAll(large);
+        return small;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number : ");

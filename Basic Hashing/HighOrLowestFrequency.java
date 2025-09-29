aa4383eae77d4e270fc02c705f7c9cc025d20a7a
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-09-29 21:55:38 +0530

    add program to check High or Low Frequency of an element in an array.

---
+++ Basic Hashing/HighOrLowestFrequency.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighOrLowestFrequency {
+    public static int[] Max_Min_Freq(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        int maxFreq = Integer.MIN_VALUE, minFreq = Integer.MAX_VALUE;
+        for (int freq : map.values()) {
+            if (freq > maxFreq)
+                maxFreq = freq;
+            if (freq < minFreq)
+                minFreq = freq;
+        }
+        int maxElem=-1,minElem=-1;
+        for (Map.Entry<Integer,Integer> e : map.entrySet()) {
+            if(e.getValue()==maxFreq && maxElem == -1) maxElem=e.getKey();
+            if(e.getValue()==minFreq && minElem == -1) minElem=e.getKey();
+        }
+        return new int[]{minElem,maxElem};
+    }
+    public static void main(String[] args) {
+        int[] arr={122,23,245,50,23,1};
+        System.out.println(Arrays.toString(Max_Min_Freq(arr)));
+    }
+}

2af07bfbc19eca742248edc64d816b0e21583fe9
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-09-29 21:11:37 +0530

    added optimized approach

--- Basic Hashing/CharacterHashing.java
+++ Basic Hashing/CharacterHashing.java
@@ -1,14 +1,37 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CharacterHashing {
-    public static void main(String[] args) {
-        String s="Asutosh";
-        HashMap<Character,Integer> map=new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        System.out.println("Frequency of 'a' : "+map.getOrDefault('a', 0));
-        System.out.println("Frequency of 'A' : "+map.getOrDefault('A', 0));
+    // public static void main(String[] args) {
+    // String s="Asutosh";
+    // HashMap<Character,Integer> map=new HashMap<>();
+    // for (char ch : s.toCharArray()) {
+    // map.put(ch, map.getOrDefault(ch, 0)+1);
+    // }
+    // System.out.println("Frequency of 'a' : "+map.getOrDefault('a', 0));
+    // System.out.println("Frequency of 'A' : "+map.getOrDefault('A', 0));
 
+    // }
+
+    // ==========OPTIMIZED APPROACH=========
+    public static Map<Character, Integer> countCharFrequency(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        String s = "Asutosh";
+        Map<Character, Integer> result = countCharFrequency(s);
+        // for (Map.Entry<Character,Integer> e : result.entrySet()) {
+        // System.out.println(e.getKey()+" --> "+e.getValue());
+        // }
+        result.forEach((key, value) -> {
+            System.out.println(key + " -->  " + value);
+        });
     }
 }
--- Basic Hashing/NumberHashing.java
+++ Basic Hashing/NumberHashing.java
@@ -1,17 +1,40 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class NumberHashing {
-    public static void main(String[] args) {
-        int[] arr = {12, 23, 21};
-        HashMap<Integer, Integer> map = new HashMap<>();
+    // public static void main(String[] args) {
+    // int[] arr = {12, 23, 21};
+    // HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Build frequency map
-        for (int i : arr) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+    // // Build frequency map
+    // for (int i : arr) {
+    // map.put(i, map.getOrDefault(i, 0) + 1);
+    // }
+
+    // // Query
+    // System.out.println("Frequency of 10 is: " + map.getOrDefault(10, 0));
+    // System.out.println("Frequency of 12 is: " + map.getOrDefault(12, 0));
+    // }
+
+    //==========optimized APPROACH =========
+    public static Map<Integer, Integer> countFrequencies(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i : nums) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+        return freq;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,1,321,2,139};
+        Map<Integer,Integer> result=countFrequencies(arr);
+
+        //print all key-value pair
+        for (Map.Entry<Integer, Integer> e : result.entrySet()) {
+            System.out.println(e.getKey()+" -->  "+e.getValue());
         }
 
-        // Query
-        System.out.println("Frequency of 10 is: " + map.getOrDefault(10, 0));
-        System.out.println("Frequency of 12 is: " + map.getOrDefault(12, 0));
+        //example query
+        System.out.println("Frequency of 2 :"+result.getOrDefault(2, 0));
     }
 }

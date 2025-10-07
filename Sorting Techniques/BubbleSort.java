a3a180046cae1502b709f34db9c636f509e41698
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-10-07 23:59:05 +0530

    add solution to bubble sort

---
+++ Sorting Techniques/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int[] nums) {
+        int n = nums.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={12,23,1,23,2,5,10};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+}

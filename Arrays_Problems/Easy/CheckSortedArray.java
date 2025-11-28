ce6695350b0b1195d18465efc558e3f4cb393dcc
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-11-28 14:50:35 +0530

    added a solution to check the array is sorted or not

---
+++ Arrays_Problems/Easy/CheckSortedArray.java
@@ -0,0 +1,21 @@
+package Arrays_Problems.Easy;
+
+import java.util.Arrays;
+
+public class CheckSortedArray {
+    public static boolean sortedOrNot(int[] arr){
+        for(int i=0;i<arr.length-2;i++){
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]<arr[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr = { 20, 12, 1, 32, 11,9 };
+        // Arrays.sort(arr);//for testing
+        System.out.println("Sorted :" + sortedOrNot(arr));
+    }
+}
--- Arrays_Problems/Easy/SecondLargest_SecondSmallest.java
+++ Arrays_Problems/Easy/SecondLargest_SecondSmallest.java
@@ -17,22 +17,24 @@ public class SecondLargest_SecondSmallest {
     }
 
     public static int secondMinSolution(int[] arr) {
-        if (arr.length < 2) return -1;
-        int min = Integer.MAX_VALUE;
-        int secMin = Integer.MAX_VALUE;
-        for (int i : arr) {
-            if (min > i) {
-                secMin = min;
-                min = i;
-            } else if (i < secMin && i != min) {
-                secMin = i;
-            }
+       if (arr.length<=2) {
+        return -1;
+       }
+       int min=Integer.MAX_VALUE;
+       int secMin=Integer.MAX_VALUE;
+       for (int i : arr) {
+        if (i < min) {
+            secMin=min;
+            min=i;
+        }if (i< secMin && i!=min) {
+            secMin=i;
         }
-        return secMin;
+       }
+       return secMin;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 20, 12, 1, 32, 11 };
+        int[] arr = { 20, 12, 1, 32, 11,9 };
         System.out.println("Second Largest Element :" + secondMaxSolution(arr));
         System.out.println("Second Smallest Element :" + secondMinSolution(arr));
     }

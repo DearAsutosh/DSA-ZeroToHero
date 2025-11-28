015683df4ec6d80647ecc8a04725fb9fb74f5a4c
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-11-28 14:55:44 +0530

    added an optimal approach for checking an array is sorted or not

--- Arrays_Problems/Easy/CheckSortedArray.java
+++ Arrays_Problems/Easy/CheckSortedArray.java
@@ -4,11 +4,19 @@ import java.util.Arrays;
 
 public class CheckSortedArray {
     public static boolean sortedOrNot(int[] arr){
-        for(int i=0;i<arr.length-2;i++){
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[j]<arr[i]) {
-                    return false;
-                }
+        //brute-force approach
+        // for(int i=0;i<arr.length-2;i++){
+            // for (int j = i+1; j < arr.length; j++) {
+            //     if (arr[j]<arr[i]) {
+            //         return false;
+            //     }
+            // }
+        // }
+
+        //optiml approach
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]<arr[i-1]) {
+                return false;
             }
         }
         return true;

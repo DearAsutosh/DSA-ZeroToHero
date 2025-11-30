fc5b493e9dd2bfeac1be772190a7020385b2ddda
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-12-01 00:37:54 +0530

    added an optimized approach for array left rotation

--- Arrays_Problems/Easy/LeftRotateAnArray.java
+++ Arrays_Problems/Easy/LeftRotateAnArray.java
@@ -5,17 +5,36 @@ import java.util.Scanner;
 
 public class LeftRotateAnArray {
     public static void Solution(int[] arr, int k) {
-        int n = arr.length;
-        k = k % n;
-        int[] rotated = new int[n];
-        for (int i = 0; i < rotated.length; i++) {
-            rotated[(k + i) % n] = arr[i];
-        }
-        for (int i = 0; i < rotated.length; i++) {
-            arr[i] = rotated[i];
-        }
-    }
+        // =======BRUTE FORCE APPROACH=========
 
+        // int n = arr.length;
+        // k = k % n;
+        // int[] rotated = new int[n];
+        // for (int i = 0; i < rotated.length; i++) {
+        //     rotated[(k + i) % n] = arr[i];
+        // }
+        // for (int i = 0; i < rotated.length; i++) {
+        //     arr[i] = rotated[i];
+        // }
+
+        // ======OPTIMIZED APPROACH===========
+        int n=arr.length;
+        k=k%n;
+        reverse(0,n-1-k,arr);
+        reverse(n-k,n-1,arr);
+        reverse(0, n-1, arr);
+
+
+    }
+public static void reverse(int i,int j,int[] nums){
+    while (i<j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
+    }
+}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = { 12, 10, 2, 25 };

4501431fe0c85a84c8d5253ee975bb302a9ba64a
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-12-03 00:57:20 +0530

    code formatted

--- Arrays_Problems/Easy/_136_singleNumber.java
+++ Arrays_Problems/Easy/_136_singleNumber.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class _136_singleNumber {
-    public static int SingleNumber(int[] nums){
-        //basic brute force approach
+    public static int SingleNumber(int[] nums) {
+        // basic brute force approach
         // Map<Integer,Integer> map=new HashMap<>();
         // for (int num : nums) {
-        //     map.put(num, map.getOrDefault(num, 0)+1);
+        // map.put(num, map.getOrDefault(num, 0)+1);
         // }
         // for (int key : map.keySet()) {
-        //     if (map.get(key)==1) {
-        //         return key;
-        //     }
+        // if (map.get(key)==1) {
+        // return key;
+        // }
         // }
         // return 0;
 
@@ -21,10 +21,11 @@ public class _136_singleNumber {
         for (int num : nums) {
             xor ^= num;
         }
-        return xor; 
+        return xor;
     }
+
     public static void main(String[] args) {
-        int[] arr1 = { 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0,2, };
+        int[] arr1 = { 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 2, };
         System.out.println("Number having single occurency :" + SingleNumber(arr1));
 
     }
